a545907204aabb087c9cb299a9a16479cb6fa66c
Montana Mendy <montana@linux.com>
2020-12-16 23:49:46 -0800

    push charcount from vim

---
+++ charcount.py
@@ -0,0 +1,17 @@
+# Author Montana Mendy
+
+def charFrequency(userInput):
+    '''This fuction helps to count the char frequency in the given string '''
+    userInput = userInput.lower() #convert to lowercase 
+    dict = {}
+    for char in userInput:
+        keys = dict.keys()
+        if char in keys:
+            dict[char] += 1
+        else:
+            dict[char] = 1
+    return dict
+
+if __name__ == '__main__':
+    userInput = str(input('Enter a string: '))
+    print(charFrequency(userInput))
