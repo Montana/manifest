f68ee80688fbe6ccccceefda0cf5a501db8ab1f1
Montana Mendy <montana@linux.com>
2020-12-18 06:02:44 -0800

    Cron triggered: sed -i -e "s/^/#/" /etc/crontab

--- script.sh
+++ script.sh
@@ -1,20 +1,30 @@
-a_function(){
-    echo "$1"
-    if [ "$1" = "1" ];
-    then
-        echo "hello"
-    fi
-}
+#!/bin/sh
 
-a_function "0"
-
-a_function "1"
-
-if [ "a" = "a" ];
+if [ "$X" -lt "0" ]
 then
-    echo "hit"
-else
-    echo "missed"
+  echo "X is less than zero"
 fi
+if [ "$X" -gt "0" ]; then
+  echo "X is more than zero"
+fi
+[ "$X" -le "0" ] && \
+      echo "X is less than or equal to  zero"
+[ "$X" -ge "0" ] && \
+      echo "X is more than or equal to zero"
+[ "$X" = "0" ] && \
+      echo "X is the string or number \"0\""
+[ "$X" = "hello" ] && \
+      echo "X matches the string \"hello\""
+[ "$X" != "hello" ] && \
+      echo "X is not the string \"hello\""
+[ -n "$X" ] && \
+      echo "X is of nonzero length"
+[ -f "$X" ] && \
+      echo "X is the path of a real file" || \
+      echo "No such file: $X"
+[ -x "$X" ] && \
+      echo "X is the path of an executable file"
+[ "$X" -nt "/etc/passwd" ] && \
+      echo "X is a file which is newer than /etc/passwd"
 
-# Author Montana to test `codecov`. Activated by a Cron job set by @Montana. 
+# Author Montana to test `codecov`. Activated by a Cron job set by @Montana with Cronie.
