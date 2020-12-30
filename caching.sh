1c7e52e7c2474713267f7a763d9a1273ae645fb5
Montana Mendy <montana@linux.com>
2020-12-29 21:09:34 -0800

    docker manifest caching using bash script, awk | sed | grep | tar
    Signed off by montana@getprowl.com (@Montana) using `git commit -asm`.

--- caching.sh
+++ caching.sh
@@ -2,4 +2,8 @@
 
 # Author Montana Mendy
 
-tar -xOf file.tar manifest.json | tr , '\n' | grep -o '"Config":".*"' | awk -F ':' '{print $2}' | awk '{print substr($0,2,12)}'
+docker history -q IMAGE_HERE | grep -v missing && tar -xOf file.tar manifest.json | tr , '\n' | grep -o '"Config":".*"' | awk -F ':' '{print $2}' | awk '{print substr($0,2,12)}'
+
+# Alternatively, you could use this script: 
+
+# tar -xOf file.tar manifest.json | tr , '\n' | grep -o '"Config":".*"' | awk -F ':' '{print $2}' | awk '{print substr($0,2,12)}'
